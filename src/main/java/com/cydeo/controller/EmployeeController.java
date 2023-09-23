bb53d31b3cd1cd1249a886cfde0be8148478974f
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-23 07:50:15 -0500

    working with thymeleaf and learning to use Model to communicate with view

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,14 +1,29 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
     @GetMapping("/register")
-    public String createEmployee(){
-        return "employee-create";
+    public String createEmployee(Model model){
+        model.addAttribute("employee",new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
+        return "employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+        DataGenerator.saveEmployee(employee);
+        model.addAttribute("employees",DataGenerator.readAllEmployees());
+
+        return "employee/employee-list";
     }
 }

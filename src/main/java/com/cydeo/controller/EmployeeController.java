6b8cc56e038b0a2a358c932b963f12786205c91f
nazik7 <59424131+nazik7@users.noreply.github.com>
2023-09-23 17:42:14 -0500

    added validation to first name in controller class

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -4,10 +4,13 @@ import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -20,7 +23,11 @@ public class EmployeeController {
     }
 
     @PostMapping("/list")
-    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("states", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
         DataGenerator.saveEmployee(employee);
         model.addAttribute("employees",DataGenerator.readAllEmployees());
 
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,10 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -11,6 +15,11 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @ToString
 public class Employee {
+    //@NotEmpty  //Except null and empty String
+    //@NotBlank // except null, empty String and only space anything can be accepted
+   // @NotNull   //except null, anything can be accepted
+    @NotBlank
+    @Size(max=12,min=2)
     private String firstName;
     private String lastName;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
